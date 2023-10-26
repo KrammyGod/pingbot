4eec99ff6d8d1d7c85ffa7e774ead5253807b2b1
KrammyGod <KrammyGod@users.noreply.github.com>
2023-10-26 16:48:04 +0000

    KrammyGod:inject-custom-client-instead

--- dist/classes/client.js
+++ dist/classes/client.js
@@ -10,27 +10,27 @@ const discord_js_1 = require("discord.js");
 function isSlashSubcommand(obj) {
     return obj && obj.data instanceof discord_js_1.SlashCommandSubcommandBuilder &&
         typeof obj.desc === 'string' && typeof obj.execute === 'function' &&
-        obj.execute.length === 2;
+        obj.execute.length <= 2;
 }
 exports.isSlashSubcommand = isSlashSubcommand;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isSlashSubcommandGroup(obj) {
     return obj && obj.data instanceof discord_js_1.SlashCommandSubcommandGroupBuilder &&
         typeof obj.desc === 'string' && typeof obj.execute === 'function' &&
-        obj.subcommands && obj.execute.length === 2;
+        obj.subcommands && obj.execute.length <= 2;
 }
 exports.isSlashSubcommandGroup = isSlashSubcommandGroup;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isSlashCommand(obj) {
     return obj && obj.data instanceof discord_js_1.SlashCommandBuilder &&
         typeof obj.desc === 'string' && typeof obj.execute === 'function' &&
-        obj.execute.length === 2;
+        obj.execute.length <= 2;
 }
 exports.isSlashCommand = isSlashCommand;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isContextCommand(obj) {
     return obj && obj.data instanceof discord_js_1.ContextMenuCommandBuilder &&
-        typeof obj.execute === 'function' && obj.execute.length === 2;
+        typeof obj.execute === 'function' && obj.execute.length <= 2;
 }
 exports.isContextCommand = isContextCommand;
 function isInteractionCommand(obj) {
