c675131d1c49a13ebcc3a79ce3441ec07f36be16
KrammyGod <shi.mark10@gmail.com>
2023-11-03 12:18:39 -0400

    Updated text output of generate character and cdn

--- src/commands/anime_commands.ts
+++ src/commands/anime_commands.ts
@@ -1696,26 +1696,26 @@ async function generateCharacterDisplay(
     let add_on = '';
     let add_emoji = ' 🆕';
     if (!character.new) {
+        add_emoji = ' 🆒';
         // CONSTANT: Refund brons
         refund = character.fc ? 2 : 1;
-        const refund_str = `+${refund} ${client.bot_emojis.brons}`;
-        add_emoji = ` 🆒 ${refund_str}`;
+        add_on = `**Received +${refund} ${client.bot_emojis.brons}**\n`;
         if (character.lvl > 1) {
-            add_emoji = ` 🆙 ${refund_str}`;
-            add_on = `**Reached level ${character.lvl}!**\n`;
+            add_emoji = ' 🆙';
+            add_on += `**Lvl (${character.lvl - 1} -> ${character.lvl})**\n`;
             // Unlocked new img
             if (character.max_img <= character.waifu!.img.length) {
                 add_on += 'You unlocked a new image! 🎉\n';
-                add_on += 'Find the waifu to switch the image!\n';
+                add_on += 'Find the character to switch the image!\n';
             }
             // Lewd mode available
             if (character.unlockedNMode) {
                 add_on += 'You unlocked a new mode! 🎉\n';
-                add_on += 'Find the waifu to switch to lewd mode!\n';
+                add_on += 'Find the character to switch to lewd mode!\n';
             // Unlocked new nimg
             } else if (character.max_nimg <= character.waifu!.nimg.length) {
                 add_on += 'You unlocked a new lewd image! 🎉\n';
-                add_on += 'Use lewd mode on the waifu to switch the image!\n';
+                add_on += 'Use lewd mode on the character to switch the image!\n';
             }
         }
     }
@@ -2421,78 +2421,6 @@ type SubmitPrivates = {
 export const submit: CachedSlashCommand<SubmissionCache> & SubmitPrivates = {
     data: new SlashCommandBuilder()
         .setName('submit')
-        .addAttachmentOption(option =>
-            option
-                .setName('image1')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image2')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image3')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image4')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image5')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image6')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image7')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image8')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('image9')
-                .setDescription('The image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd1')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd2')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd3')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd4')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd5')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd6')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd7')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd8')
-                .setDescription('The lewd image of the character.'))
-        .addAttachmentOption(option =>
-            option
-                .setName('lewd9')
-                .setDescription('The lewd image of the character.'))
         .setDescription('Create a submission request to add a character.'),
 
     desc: 'Want to add a character that is not currently in the starred database?\n' +
@@ -2503,14 +2431,9 @@ export const submit: CachedSlashCommand<SubmissionCache> & SubmitPrivates = {
           '2. If the character is from an **anime** that already exists, use the anime searcher.\n' +
           "3. The character's **gender** must be one of: `Male`, `Female`, `Unknown`.\n" +
           '4. If its a new character, the character must have at least **one normal image**.\n' +
-          '5. Optionally, if you would like, you can add a file to the `image` option or `lewd` option (up to 9).\n' +
-          'These links will then be prepopulated in the modal. **DO NOT CHANGE THESE.**\n\n' +
-          'Note that you can only submit one file per option at time. This is a discord limitation.\n\n' +
-          'Usage: `/submit image(1-9): [file] lewd(1-9): [file]`\n\n' +
-          '__**Options**__\n' +
-          '*image(1-9):* The normal image(s) to add to the character.\n' +
-          '*lewd(1-9):* The lewd image(s) to add to the character.\n\n' +
-          'Examples: `/submit`, `/submit image1: girl.png image2: girl2.png lewd1: girllewd.png`',
+          'When using submit character or anime, values will be prepopulated in the modal. ' +
+          '**DO NOT CHANGE THESE.**\n\n' +
+          'Usage: `/submit`',
 
     // We can't use this.data.name
     cache: new DB.Cache('submit'),
@@ -2962,8 +2885,7 @@ export const submit: CachedSlashCommand<SubmissionCache> & SubmitPrivates = {
             // Selected, we can submit.
             if (i.customId === 'selectWaifu') {
                 return this.startSubmit(i, waifu);
-            }
-            if (i.customId === 'searchWaifu') {
+            } else if (i.customId === 'searchWaifu') {
                 // Search for waifu
                 const modal = new ModalBuilder({
                     title: 'Waifu Search',
@@ -3053,16 +2975,6 @@ export const submit: CachedSlashCommand<SubmissionCache> & SubmitPrivates = {
 
     async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
-        const img = [];
-        for (let i = 0; i < 9; ++i) {
-            const attachment = interaction.options.getAttachment(`image${i + 1}`);
-            if (attachment) img.push(attachment.url);
-        }
-        const nimg = [];
-        for (let i = 0; i < 9; ++i) {
-            const attachment = interaction.options.getAttachment(`lewd${i + 1}`);
-            if (attachment) nimg.push(attachment.url);
-        }
-        return this.startSelector(interaction, img, nimg);
+        return this.startSelector(interaction, [], []);
     }
 };
