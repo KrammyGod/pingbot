55f6c268268d14b511ff0c28f93d349c4c03102b
KrammyGod <shi.mark10@gmail.com>
2024-04-29 17:12:30 -0400

    Set node_env to be production to allow npm install to function without omit

--- src/classes/config.ts
+++ src/classes/config.ts
@@ -79,5 +79,5 @@ export default {
     /**
      * The environment the bot is running in. Only used to determine whether to send a welcome message or not.
      */
-    env: process.env.NODE_ENV,
+    env: process.env.BOT_ENV,
 };
