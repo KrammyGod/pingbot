bf3b14a9c2c64381d0e23945c34ff848734e70e6
KrammyGod <shi.mark10@gmail.com>
2024-12-19 13:59:26 -0500

    feat: temporarily re-enable music commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,5 +4,4 @@ export * as fun from './fun_commands';
 export * as help from './help_command';
 export * as minigame from './minigame_commands';
 export * as mod from './mod_commands';
-// Disabling all music commands atm, there is currently an issue with the player interacting with youtube's API
-// export * as music from './music_commands';
+export * as music from './music_commands';
