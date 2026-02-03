906403ea154a86f85da5ccb935bf06bc9feb8a8b
KrammyGod <34082435+KrammyGod@users.noreply.github.com>
2026-02-03 01:35:39 +0000

    Merge pull request #154
    fix(skport): autocollector now checks signature

--- dist/collector/skport.js
+++ dist/collector/skport.js
@@ -1,11 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SkportCollector = void 0;
+const crypto_1 = __importDefault(require("crypto"));
 const cookie_1 = require("cookie");
 const CONFIG = {
     rewardURL: process.env.rewardURL,
     roleURL: process.env.roleURL,
     signURL: process.env.signURL,
+    refreshURL: process.env.refreshURL,
+    platform: process.env.platform,
+    vName: process.env.vName,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
         'Chrome/114.0.0.0 Safari/537.36',
 };
@@ -16,21 +23,57 @@ class SkportCollector {
         this.uid = uid;
         this.LOGGER = LOGGER;
         this.extraHeaders = {};
+        this.token = '';
         // Extract the required values from our custom made cookie.
         const vals = (0, cookie_1.parse)(this.cookie ?? '');
         // The actual credential for signing in.
         this.extraHeaders.cred = vals.cred;
-        // The user's game ID
+        // The user's private game ID
         this.extraHeaders['Sk-Game-Role'] = vals['sk-game-role'];
     }
-    get header() {
+    /**
+     * Credits to this comment for finding how to sign:
+     * https://gist.github.com/cptmacp/1e9a9f20f69c113a0828fea8d13cb34c?permalink_comment_id=5967252#gistcomment-5967252
+     */
+    generateSign(path, body, timestamp, token) {
+        let str = path + body + timestamp;
+        // eslint-disable-next-line @stylistic/js/max-len
+        const headerJson = `{"platform":"${CONFIG.platform}","timestamp":"${timestamp}","dId":"","vName":"${CONFIG.vName}"}`;
+        str += headerJson;
+        // Sign using the token obtained from refresh
+        const hmacBytes = crypto_1.default.createHmac('sha256', token).update(str).digest();
+        // MD5 the SHA256 HMAC result
+        const md5Bytes = crypto_1.default.createHash('md5').update(hmacBytes.toString('hex')).digest();
+        return md5Bytes.toString('hex');
+    }
+    /**
+     * Base headers required for every request.
+     * Refresh token must use only these headers to avoid infinite recursion.
+     */
+    get baseHeaders() {
         return {
             'Accept': 'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
             'User-Agent': CONFIG.userAgent,
-            'x-rpc-app_version': '2.34.1',
-            'x-rpc-client_type': '4',
+            'platform': CONFIG.platform,
+            'vName': CONFIG.vName,
+            'Origin': 'https://game.skport.com',
+            'Referer': 'https://game.skport.com/',
+            ...this.extraHeaders,
+        };
+    }
+    /**
+     * Get headers for every request other than refresh tokens.
+     * This signs the request using the request token retrieved at the time of signing.
+     */
+    async getHeaders(path, body) {
+        await this.refreshToken();
+        const timestamp = Math.floor(Date.now() / 1000).toString();
+        const sign = this.generateSign(path, body, timestamp, this.token);
+        return {
+            'sign': sign,
+            'timestamp': timestamp,
+            ...this.baseHeaders,
             ...this.extraHeaders,
         };
     }
@@ -56,8 +99,21 @@ class SkportCollector {
             cnt: resource.count.toString(),
         };
     }
+    async refreshToken() {
+        return fetch(CONFIG.refreshURL, { headers: this.baseHeaders })
+            .then(res => res.json())
+            .then((data) => {
+            this.token = data.data?.token;
+        })
+            .catch(err => {
+            this.LOGGER.error('failure in token refresh');
+            throw err;
+        });
+    }
     async getAwards() {
-        return fetch(CONFIG.rewardURL, { headers: this.header })
+        const path = URL.parse(CONFIG.rewardURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
+        return fetch(CONFIG.rewardURL, { headers })
             .then(res => res.json())
             .then((data) => {
             if (data.code !== 0) {
@@ -72,7 +128,9 @@ class SkportCollector {
         });
     }
     async getInfo() {
-        const res = await fetch(CONFIG.roleURL, { headers: this.header })
+        const path = URL.parse(CONFIG.roleURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
+        const res = await fetch(CONFIG.roleURL, { headers })
             .then(res => res.json())
             .catch(err => {
             this.LOGGER.error('failure in get role response');
@@ -103,9 +161,11 @@ class SkportCollector {
     async run() {
         this.LOGGER.log('Running sign in...');
         if (!this.notify) {
+            const path = URL.parse(CONFIG.signURL)?.pathname ?? '/';
+            const headers = await this.getHeaders(path, '');
             return fetch(CONFIG.signURL, {
                 method: 'POST',
-                headers: { ...this.header, 'Content-Type': 'application/json' },
+                headers: { ...headers, 'Content-Type': 'application/json' },
             }).then(res => res.json()).then((data) => {
                 const code = data.code;
                 if (code || code !== 0) {
@@ -141,11 +201,13 @@ class SkportCollector {
             result.check_in_result = '❎ Already checked in today';
             return result;
         }
+        const path = URL.parse(CONFIG.signURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
         const res = await fetch(CONFIG.signURL, {
             method: 'POST',
-            headers: { ...this.header, 'Content-Type': 'application/json' },
+            headers: { ...headers, 'Content-Type': 'application/json' },
         }).then(res => res.json());
-        // Checking for last minute failures/anti-bot
+        // Checking for last minute failures
         if (res.code !== 0) {
             // Usually cookie error or something similar
             this.LOGGER.error('Error in check-in.');
