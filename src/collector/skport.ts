91ecc6237a0eff8cc320c5205541b1ef40dcd159
KrammyGod <shi.mark10@gmail.com>
2026-02-02 20:11:03 -0500

    fix(skport): autocollector now checks signature

--- src/collector/skport.ts
+++ src/collector/skport.ts
@@ -1,5 +1,7 @@
-import type { CollectBase, CollectResult, Logger } from './collect';
+import crypto from 'crypto';
 import { parse } from 'cookie';
+
+import type { CollectBase, CollectResult, Logger } from './collect';
 
 type AwardCalendar = {
     readonly awardId: string; // ID is linked to resourceInfoMap
@@ -12,6 +14,15 @@ type AwardResource = {
     readonly count: number;
     readonly name: string;
     readonly icon: string;
+};
+
+type RefreshAPIResponse = {
+    readonly code: number;
+    readonly message: string;
+    readonly timestamp: string;
+    readonly data: {
+        readonly token: string;
+    };
 };
 
 type RewardAPIResponse = {
@@ -72,12 +83,16 @@ const CONFIG = {
     rewardURL: process.env.rewardURL!,
     roleURL: process.env.roleURL!,
     signURL: process.env.signURL!,
+    refreshURL: process.env.refreshURL!,
+    platform: process.env.platform!,
+    vName: process.env.vName!,
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' +
         'Chrome/114.0.0.0 Safari/537.36',
 };
 
 export class SkportCollector implements CollectBase {
     private readonly extraHeaders: Record<string, string> = {};
+    private token: string = '';
     constructor(
         private readonly cookie: string,
         private readonly notify: boolean,
@@ -88,18 +103,62 @@ export class SkportCollector implements CollectBase {
         const vals = parse(this.cookie ?? '');
         // The actual credential for signing in.
         this.extraHeaders.cred = vals.cred as string;
-        // The user's game ID
+        // The user's private game ID
         this.extraHeaders['Sk-Game-Role'] = vals['sk-game-role'] as string;
     }
 
-    get header() {
+    /**
+     * Credits to this comment for finding how to sign:
+     * https://gist.github.com/cptmacp/1e9a9f20f69c113a0828fea8d13cb34c?permalink_comment_id=5967252#gistcomment-5967252
+     */
+    generateSign(
+        path: string,
+        body: string,
+        timestamp: string,
+        token: string,
+    ) {
+        let str = path + body + timestamp;
+        // eslint-disable-next-line @stylistic/js/max-len
+        const headerJson = `{"platform":"${CONFIG.platform}","timestamp":"${timestamp}","dId":"","vName":"${CONFIG.vName}"}`;
+        str += headerJson;
+
+        // Sign using the token obtained from refresh
+        const hmacBytes = crypto.createHmac('sha256', token).update(str).digest();
+
+        // MD5 the SHA256 HMAC result
+        const md5Bytes = crypto.createHash('md5').update(hmacBytes.toString('hex')).digest();
+        return md5Bytes.toString('hex');
+    }
+
+    /**
+     * Base headers required for every request.
+     * Refresh token must use only these headers to avoid infinite recursion.
+     */
+    get baseHeaders() {
         return {
             'Accept': 'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
             'User-Agent': CONFIG.userAgent,
-            'x-rpc-app_version': '2.34.1',
-            'x-rpc-client_type': '4',
+            'platform': CONFIG.platform,
+            'vName': CONFIG.vName,
+            'Origin': 'https://game.skport.com',
+            'Referer': 'https://game.skport.com/',
+            ...this.extraHeaders,
+        };
+    }
+
+    /**
+     * Get headers for every request other than refresh tokens.
+     * This signs the request using the request token retrieved at the time of signing.
+     */
+    async getHeaders(path: string, body: string) {
+        await this.refreshToken();
+        const timestamp = Math.floor(Date.now() / 1000).toString();
+        const sign = this.generateSign(path, body, timestamp, this.token);
+        return {
+            'sign': sign,
+            'timestamp': timestamp,
+            ...this.baseHeaders,
             ...this.extraHeaders,
         };
     }
@@ -129,8 +188,22 @@ export class SkportCollector implements CollectBase {
         };
     }
 
+    async refreshToken() {
+        return fetch(CONFIG.refreshURL, { headers: this.baseHeaders })
+            .then(res => res.json())
+            .then((data: RefreshAPIResponse) => {
+                this.token = data.data?.token;
+            })
+            .catch(err => {
+                this.LOGGER.error('failure in token refresh');
+                throw err;
+            });
+    }
+
     async getAwards() {
-        return fetch(CONFIG.rewardURL, { headers: this.header })
+        const path = URL.parse(CONFIG.rewardURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
+        return fetch(CONFIG.rewardURL, { headers })
             .then(res => res.json())
             .then((data: RewardAPIResponse) => {
                 if (data.code !== 0) {
@@ -146,7 +219,9 @@ export class SkportCollector implements CollectBase {
     }
 
     async getInfo(): Promise<{uid: string, serverName: string, nickname: string}> {
-        const res = await fetch(CONFIG.roleURL, { headers: this.header })
+        const path = URL.parse(CONFIG.roleURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
+        const res = await fetch(CONFIG.roleURL, { headers })
             .then(res => res.json() as Promise<RoleAPIResponse>)
             .catch(err => {
                 this.LOGGER.error('failure in get role response');
@@ -178,9 +253,11 @@ export class SkportCollector implements CollectBase {
     async run(): Promise<CollectResult | undefined> {
         this.LOGGER.log('Running sign in...');
         if (!this.notify) {
+            const path = URL.parse(CONFIG.signURL)?.pathname ?? '/';
+            const headers = await this.getHeaders(path, '');
             return fetch(CONFIG.signURL, {
                 method: 'POST',
-                headers: { ...this.header, 'Content-Type': 'application/json' },
+                headers: { ...headers, 'Content-Type': 'application/json' },
             }).then(res => res.json()).then((data: SignAPIResponse) => {
                 const code = data.code;
                 if (code || code !== 0) {
@@ -217,12 +294,14 @@ export class SkportCollector implements CollectBase {
             return result;
         }
 
+        const path = URL.parse(CONFIG.signURL)?.pathname ?? '/';
+        const headers = await this.getHeaders(path, '');
         const res = await fetch(CONFIG.signURL, {
             method: 'POST',
-            headers: { ...this.header, 'Content-Type': 'application/json' },
+            headers: { ...headers, 'Content-Type': 'application/json' },
         }).then(res => res.json() as Promise<SignAPIResponse>);
 
-        // Checking for last minute failures/anti-bot
+        // Checking for last minute failures
         if (res.code !== 0) {
             // Usually cookie error or something similar
             this.LOGGER.error('Error in check-in.');
